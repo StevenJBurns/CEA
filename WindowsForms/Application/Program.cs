688ac3ae2aa0e191f9bd22e96f3427578387d72c
StevenJBurns <stevenjburns@msn.com>
2016-08-12 23:24:51 -0600

    Add project files.

---
+++ WindowsForms/Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsForms
+  {
+  static class CEA
+    {
+    /// <summary>/// The main entry point for the application./// </summary>
+    [STAThread]
+    static void Main()
+      {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+      }
+    }
+ }

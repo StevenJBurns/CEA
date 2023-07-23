fa830424f13f1f0ce444cd036c51342b0a762313
sjb <stevenjburns@msn.com>
2023-07-23 07:31:33 -0600

    chore: formatting clean up in enum OfficerGrades

--- CEA.Domain/Crewmember/Enum/OfficerGrades.cs
+++ CEA.Domain/Crewmember/Enum/OfficerGrades.cs
@@ -1,13 +1,13 @@
-﻿namespace CEA.Domain.Crewmember
+﻿namespace CEA.Domain.Crewmember;
+
+public enum OfficerGrades
   {
-  public enum OfficerGrades
-	{
-    Unknown = 0,
-    O1,
-    O2,
-    O3,
-    O4,
-    O5,
-    O6
-	}
+  Unknown = 0,
+  O1,
+  O2,
+  O3,
+  O4,
+  O5,
+  O6
   }
+
